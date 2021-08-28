08d373d84f8c670baaaafca0ddaa3cbe66b5bd1d
Tomdzsó <tomdzso@tomdzsolabs.tk>
2021-08-28 23:32:15 +0200

    fixed accents

--- api/private/scrapeOriginal.py
+++ api/private/scrapeOriginal.py
@@ -4,17 +4,19 @@
 from http.server import BaseHTTPRequestHandler
 import os
 import requests
-# mindenhova beférkőzik ez a levesember. igazából engem is megvett kilóra, ez a projekt az ő megrendelésére készül szigorúan szabad piaci alapon.
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # mindenhova beférkőzik ez a levesember. igazából engem is megvett kilóra, ez a projekt az ő megrendelésére készül szigorúan szabad piaci alapon.
 from io import BytesIO as IO
 import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from unidecode import unidecode  # erre igazán írhattam volna egy függvényt magamnak
+
 
 def handleParagraph(paragraph):
     if "Jelölő szervezet:" in str(paragraph):
-        return "party", [i.lower() for i in paragraph.findChildren("strong")[0].next_sibling.strip().split("-")]
+        return "party", [unidecode(i.lower()) for i in paragraph.findChildren("strong")[0].next_sibling.strip().split("-")]
+
     keys = {
         "nyilvános önéletrajz": "cv",
         "vagyonnyilatkozat": "declarationOfAssets",
@@ -23,16 +25,17 @@ def handleParagraph(paragraph):
         "értéknyilatkozat": "declarationOfValues",
         "nyilvántartásba vételi határozat": "decisionOfRegistration"
     }
-    
+
     documents = {}
     for i in paragraph.findChildren("a"):
         try:
             documents[keys[i.getText()]] = i["href"]
         except KeyError:
             pass
-    
+
     if documents:
         return "documents", documents
+
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -45,7 +48,7 @@ class handler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.end_headers()
-        
+
         firebase_admin.initialize_app(credentials.Certificate("./api/key.json"))
         database = firestore.client()
 
@@ -59,22 +62,22 @@ class handler(BaseHTTPRequestHandler):
                 link = candidateContainer.findChildren("a")[0]["href"]
             except IndexError:
                 continue
-            
+
             candidate["image"] = candidateContainer.findChildren("img", recursive=True)[0]["src"]
-            
+
             currentCandidateSoup = BeautifulSoup(requests.get(link).content, "html.parser").find("article")
             currentCandidateContent = currentCandidateSoup.findChildren("div", {"class": "entry-content"})[0]
-            try: 
+            try:
                 candidate["name"] = currentCandidateSoup.findChildren("h1", {"class": "entry-title"})[0].getText()
             except IndexError:
                 pass
-            
+
             for paragraph in currentCandidateContent.findChildren("p"):
                 result = handleParagraph(paragraph)
                 if result:
                     candidate[result[0]] = result[1]
-            
+
             collection.document(str(index)).set(candidate)
-        
+
         self.wfile.write("done".encode())
         return
